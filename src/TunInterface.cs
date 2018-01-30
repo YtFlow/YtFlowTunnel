8743dcd08186606c33b426e3ab0e79c46e6b1ef4
bdbai <bdbaiapp@163.com>
2018-01-30 15:08:29 +0800

    Initial commit

---
+++ src/TunInterface.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Wintun2socks;
+
+namespace YtFlowTunnel
+{
+    public delegate void PacketPopedHandler(object sender, [ReadOnlyArray] byte[] e);
+    public sealed class TunInterface
+    {
+        ConcurrentQueue<Action> dispatchQ = new ConcurrentQueue<Action>();
+        Task dispatchWorker;
+        Task worker;
+        EventWaitHandle dispatchWaitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+        EventWaitHandle timeoutWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
+        Wintun w = Wintun.Instance;
+        Action<string> Write;
+        Action<string> WriteLine;
+
+        public event PacketPopedHandler PacketPoped;
+
+        internal void executeLwipTask(Action act)
+        {
+            dispatchQ.Enqueue(act);
+            dispatchWaitHandle.Set();
+        }
+        public void Init()
+        {
+            WriteLine = Write = str =>
+            {
+                // logger(str);
+                //return null;
+            };
+            dispatchWorker = Task.Run(() =>
+            {
+                while (true)
+                {
+                    Debug.WriteLine($"{dispatchQ.Count} tasks remain");
+                    while (dispatchQ.TryDequeue(out Action act))
+                    {
+                        timeoutWaitHandle.Reset();
+                        worker = Task.Run(() =>
+                        {
+                            act();
+                            timeoutWaitHandle.Set();
+                        });
+                        timeoutWaitHandle.Reset();
+                        timeoutWaitHandle.WaitOne(2000);
+                    }
+                    dispatchWaitHandle.Reset();
+                    dispatchWaitHandle.WaitOne();
+                }
+            });
+
+            w.PacketPoped += W_PopPacket;
+            TcpSocket.EstablishedTcp += W_EstablishTcp;
+
+            w.Init();
+        }
+
+        private void W_EstablishTcp(TcpSocket socket)
+        {
+            Debug.WriteLine($"{TcpSocket.ConnectionCount()} connections now");
+            // ShadowsocksR server with procotol=origin, method=none
+            RawShadowsocksAdapter a = new RawShadowsocksAdapter("80.80.80.80", 1234, socket, this);
+        }
+
+        private void W_PopPacket(object sender, byte[] e)
+        {
+            PacketPoped(sender, e);
+        }
+
+        public void PushPacket([ReadOnlyArray] byte[] packet)
+        {
+            executeLwipTask(() => w.PushPacket(packet));
+        }
+    }
+}
---
+++ src/VpnTask.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using Windows.Networking.Vpn;
+
+namespace YtFlowTunnel
+{
+    public sealed class VpnTask: IBackgroundTask
+    {
+        private static IVpnPlugIn _pluginInstance = null;
+        private static object _pluginLocker = new object();
+        public static IVpnPlugIn GetPlugin(BackgroundTaskDeferral def)
+        {
+            if (_pluginInstance == null)
+            {
+                lock (_pluginLocker)
+                {
+                    if (_pluginInstance != null) return _pluginInstance;
+                    _pluginInstance = new VpnPlugin(def);
+                }
+            }
+            return _pluginInstance;
+        }
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine("Running");
+            var def = taskInstance.GetDeferral();
+            VpnChannel.ProcessEventAsync(GetPlugin(def), taskInstance.TriggerDetails);
+        }
+    }
+}
